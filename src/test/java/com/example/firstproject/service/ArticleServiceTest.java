a2e50a066627ee2ea71884f1f9918c0b1052a2f5
wlfwngo1004 <109897248+wlfwngo1004@users.noreply.github.com>
2023-10-06 20:49:19 +0900

    테스트 코드
    테스트 코드 초반

---
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.firstproject.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ArticleServiceTest {
+    @Autowired
+    ArticleService articleService;
+
+    @Test
+    void index() {
+    }
+}
